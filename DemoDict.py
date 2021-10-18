e28b81ceb730a2484b00db12fac9f54711c3e99d
zoomsun00 <zoomsunup@gmail.com>
2021-10-18 15:28:32 +0900

    함수연습1

--- DemoDict.py
+++ DemoDict.py
@@ -14,3 +14,25 @@ for item in device.items():
 print("---key, valude---")
 for k,v in device.items():
     print(k,v)
+
+phone={"kim": "1111", "lee":"2222"}
+# 참조를 복사해서 전달
+p=phone
+print(id(phone),id(p) )
+
+
+#교집합을 구하는 함수
+def intersect(prelist, postlist):
+    #c참수 내부에만 있는 지역 변수
+    result=[]
+    # 단어의 글자를 슬라이싱 하는 코드:H(0),| A(1) | M(2)
+    for x in prelist:
+        #postlist에 있고 result에는 없는 글자면 추가
+        if x in postlist and x not in result:
+            result.append(x)
+    # 루프 다 돌고 나서 리턴
+    return result
+
+#함수 호출
+print( intersect("HAM", "SPAM") )
+
