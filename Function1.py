85a6f0c90f75e3ba24e88c02dfc4d772804f72f4
zoomsun00 <zoomsunup@gmail.com>
2021-10-18 15:44:53 +0900

    함수연습2

---
+++ Function1.py
@@ -0,0 +1,15 @@
+# Function1.py
+# 함수를 먼저 정의
+def setValues(a):
+    x = a 
+    print(x)
+
+#함수를 호출
+result = setValues(5)
+print(result)
+
+def swaq(x,y):
+    return y,x
+
+#호출
+print( swap(5,6) )
