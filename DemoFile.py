e513216b503ec56bd8e2b0e004c6cc001b50d05f
zoomsun00 <zoomsunup@gmail.com>
2021-10-20 10:38:00 +0900

    파일연습2

--- DemoFile.py
+++ DemoFile.py
@@ -15,8 +15,33 @@ for x in range(1,6):
 for x in range(1,6):
  print(x,"*", x, "=", str(x*x).zfill(5) )
 
-print("{0}:x}".format(10))
-print("{0}:b}".format(10))
-print("{0}:f}".format(4/3))
-print("{0}:.2f}".format(4/3))
-print("{0}:,}".format(15000000))
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+print("{0:f}".format(4/3))
+print("{0:.2f}".format(4/3))
+print("{0:,}".format(15000000))
+
+
+#파일쓰기
+f = open("c:\\work\\domo.txt", "wt", encoding="utf-8")
+f.write("첫번째라인\n두번째\n세번째\n")
+f.close()
+
+if f.closed:
+    print("파일이 정상 종료")
+else:
+    f.close()
+
+#파일읽기
+f = open("c:\\work\\domo.txt", "rt", encoding="utf-8")
+print( f.read() )
+print( f.tell() )
+f.seek(0)
+print("--한줄씩 읽기---")
+print( f.readline() )
+print( f.readline() )
+f.seek(0)
+print("--list로 받기---")
+result = f.readlines()
+print( result )
+f.close()
