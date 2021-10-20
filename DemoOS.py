afce45c46c31933a6c295195959df56d4b798cf2
zoomsun00 <zoomsunup@gmail.com>
2021-10-20 14:36:32 +0900

    운영체제 파일리스트

---
+++ DemoOS.py
@@ -0,0 +1,20 @@
+# DemoOS.py
+from os.path import *
+
+print( abspath("python.exe") )
+print( basename("c:\\python38\\python.exe") )
+print( getsize("c:\\python38\\python.exe") )
+print( exists("c:\\python38\\python.exe") )
+
+#운영체제에 있는 기능 접근: cd \ cd work
+from os import *
+print ( name )
+print (environ.keys() )
+print( getcwd() )
+chdir("..")
+print( getcwd() )
+chdir("c:\\work")
+
+# dir, ls 명령어
+import glob
+print ( glob.glob("*.py"))
